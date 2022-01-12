bb1993a3967da223906b28a9ef728f5f2be83f1a
Your Name <you@example.com>
2022-01-13 01:06:33 +0600

    dads

--- app/providers/api.py
+++ app/providers/api.py
@@ -8,14 +8,15 @@ from fastapi.responses import HTMLResponse,RedirectResponse
 from app.providers.emaildb import EmailRedis
 result_router = APIRouter()
 
-@result_router.get("/{provider}", status_code=201,response_class=HTMLResponse)
-async def auth_provider(provider:str,data: AuthModel = Depends()):
-    html_content=open(f'app/providers/{provider}/page.html').read()
-    return HTMLResponse(content=html_content, status_code=200)
-
 @result_router.get("/result", status_code=201,response_class=RedirectResponse)
 async def return_result(data: ResultModel = Depends()):
     email=(EmailRedis().get_email(data.provider,data.id))
     if not email:
         return RedirectResponse(url='https://github.com/tiangolo/fastapi/issues/199')
     return '200'
+
+@result_router.get("/{provider}", status_code=201,response_class=HTMLResponse)
+async def auth_provider(provider:str,data: AuthModel = Depends()):
+    html_content=open(f'app/providers/{provider}/page.html').read()
+    return HTMLResponse(content=html_content, status_code=200)
+
