da2d3ac8d4aee0673f5e3efc0f4cd5886d00655d
Your Name <you@example.com>
2022-01-13 00:45:28 +0600

    dsad

--- app/providers/vk/api.py
+++ app/providers/vk/api.py
@@ -4,7 +4,7 @@
 from fastapi import APIRouter, HTTPException, Path, FastAPI,Depends
 from typing import List 
 from app.providers.models import AuthModel,VkAuthModel
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse,RedirectResponse
 from app.providers.emaildb import EmailRedis
 vk_router = APIRouter()
 @vk_router.get("/", status_code=201,response_class=HTMLResponse)
@@ -12,7 +12,9 @@ async def create_note(data: AuthModel = Depends()):
     html_content=open('app/providers/vk/page.html').read()
     return HTMLResponse(content=html_content, status_code=200)
 
-@vk_router.get("/result", status_code=201,response_class=HTMLResponse)
+@vk_router.get("/result", status_code=201,response_class=RedirectResponse)
 async def create_note(data: VkAuthModel = Depends()):
-    print(EmailRedis().get_email('VK',data.id))
+    email=(EmailRedis().get_email('VK',data.id))
+    if not email:
+        return RedirectResponse(url='https://github.com/tiangolo/fastapi/issues/199')
     return '200'
