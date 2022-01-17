a6d0a552271287e411c724eaa18a20651d0ba07d
Your Name <you@example.com>
2022-01-17 23:00:00 +0600

    dasdsa

--- app/providers/emaildb.py
+++ app/providers/emaildb.py
@@ -3,6 +3,6 @@ class EmailRedis:
     def __init__(self):
         self.db=Redis().db
     def get_email(self,provider,id):
-        return str(self.db.hget(f"{provider}", id))
+        return str(self.db.hget(f"{provider}", id).decode())
     def set_email(self,provider,id,email):
         self.db.hset(f"{provider}", id, email)
